42ceffb0cb82395bb39b46123d4e968c58582a84
Junsang Mun <mun.js@aol.com>
2022-11-16 14:46:49 +0900

    [ADD] api routing

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,4 +3,14 @@ import express from 'express';
 const router = express.Router();
 export { router as apiRouter };
 
-router.get('/');
+router.get('/list', async function(req, res) {
+	const data = 'data'; //do something... query a data...
+	res.status(200);
+	res.send(data);
+});
+
+router.get('/query', async function (req, res) {
+	const data = 'data'; //do something... query a data...
+	res.status(200);
+	res.send(data);
+});
